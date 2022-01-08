34f36f9b42f74e3ba139e3a9cd151b7e13f1d081
frankpjr1 <34253047+frankpjr1@users.noreply.github.com>
2022-01-07 21:15:37 -0500

    first commit for arrays and objects

---
+++ app.js
@@ -0,0 +1,33 @@
+
+
+let users = [{
+    Name: "Philip Nelson",
+    Address: "71723 Jaskolski Valleys Tiffanyburgh, AL 49229-7949",
+    Email: "kemmer.corbin@ondricka.com",
+    Password: "1d0b20b2",
+}, {
+    Name: "Veronica Johnson",
+    Address: "37883 Bernier Harbors Abelburgh, KS 71420-7448",
+    Email: "cronin.pauline@shields.info",
+    Password: "6d0b30b6",
+}, {
+    Name: "Gabriela Diaz",
+    Address: "679 Towne Flats Apt. 376 Brendanchester, TN 23251-8361",
+    Email: "rbatz@yahoo.com",
+    Password: "7c0b30b6",
+}, {
+    Name: "Adelynn Cox",
+    Address: "780 Delbert Walks West Vito, NC 53825-2013",
+    Email: "herminia.hauck@gmail.com",
+    Password: "2d0b30b4",
+}, {
+    Name: "Camilla Morales",
+    Address: "35505 Hills Mountain Apt. 742South Selina, OH 52624",
+    Email: "herminia.hauck@gmail.com",
+    Password: "0a0b30w6",
+}]
+
+for (const arrkey in users){
+    console.log(arrkey,users[arrkey]);
+
+}
